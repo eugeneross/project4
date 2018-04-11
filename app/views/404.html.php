e8999abc063abe9e812f08a40007db1a15d9e7dc
Eugene Ross <e@eugeneross.com>
2018-04-11 09:56:06 -0400

    blog

---
+++ app/views/404.html.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+
+	<title>404 Not Found | <?php echo config('blog.title') ?></title>
+	<link href="<?php echo site_url() ?>assets/css/style.css" rel="stylesheet" />
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|PT+Serif:700" rel="stylesheet">
+
+</head>
+<body>
+
+	<div class="center message">
+		<h1>This page doesn't exist!</h1>
+		<p>Would you like to go to our <a href="<?php echo site_url() ?>">homepage</a> instead?</p>
+	</div>
+
+</body>
+</html>
---
+++ vendor/autoload.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once __DIR__ . '/composer' . '/autoload_real.php';
+
+return ComposerAutoloaderInit352930101c1dc3fb72305f7934017105::getLoader();
